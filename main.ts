8fbf09eaa064c691861db34c7f863292312dfb06
Daph21 <82120108+Daph21@users.noreply.github.com>
2022-06-11 11:27:02 -0400

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,30 @@
-let strip = neopixel.create(DigitalPin.P1, 8, NeoPixelMode.RGB)
-strip.showRainbow(1, 360)
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(pression)
+})
+let pression = 0
+let strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
+strip.clear()
+strip.show()
+pins.servoWritePin(AnalogPin.P2, 180)
 basic.forever(function () {
-    strip.rotate(1)
-    strip.show()
+    pression = pins.analogReadPin(AnalogPin.P1)
+    if (pression > 400) {
+        strip.showRainbow(1, 360)
+        basic.showNumber(5)
+        for (let index = 0; index < 16; index++) {
+            strip.rotate(1)
+            strip.show()
+            basic.pause(100)
+        }
+    } else {
+        strip.clear()
+        strip.show()
+        basic.showIcon(IconNames.Sad)
+    }
+})
+basic.forever(function () {
+    pins.servoWritePin(AnalogPin.P2, 120)
+    basic.pause(500)
+    pins.servoWritePin(AnalogPin.P2, 0)
+    basic.pause(500)
 })
